c6b487910b9983fc80a49d0009b300e27ed537a1
happylrd <happylrd17@gmail.com>
2017-06-29 20:18:24 +0800

    Add user api
    with response obj

---
+++ Iris/src/main/java/io/happylrd/youbo/controller/UserController.java
@@ -0,0 +1,42 @@
+package io.happylrd.youbo.controller;
+
+import io.happylrd.youbo.common.ServerResponse;
+import io.happylrd.youbo.domain.User;
+import io.happylrd.youbo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    public ServerResponse<List<User>> list() {
+        return ServerResponse.createBySuccess(userRepository.findAll());
+    }
+
+    @PostMapping(value = "/users")
+    public ServerResponse<User> save(User user) {
+        return ServerResponse.createBySuccess(userRepository.save(user));
+    }
+
+    @GetMapping(value = "/users/{username}")
+    public ServerResponse<User> getByUsername(@PathVariable("username") String username) {
+        return ServerResponse.createBySuccess(userRepository.findByUsername(username));
+    }
+
+    @PutMapping(value = "/users/{id}")
+    public ServerResponse<User> update(@PathVariable("id") Long id,
+                                       @RequestBody User user) {
+        return ServerResponse.createBySuccess(userRepository.save(user));
+    }
+
+    @DeleteMapping(value = "/users/{id}")
+    public void remove(@PathVariable("id") Long id) {
+        userRepository.delete(id);
+    }
+}
---
+++ Iris/src/main/java/io/happylrd/youbo/domain/User.java
@@ -0,0 +1,156 @@
+package io.happylrd.youbo.domain;
+
+import io.happylrd.youbo.common.Const;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, length = 30, nullable = false, updatable = false)
+    private String username;
+
+    @Column(length = 128, nullable = false)
+    private String password;
+
+    @Column(unique = true, length = 20)
+    private String nickname;
+
+    @Column(length = 10)
+    private String realname;
+
+    @Column(length = 11)
+    private String mobile;
+
+    private String avatarUrl;
+
+    /**
+     * Current constraint: `male` for 男生, `female` for 女生, `unknown` for 未知.
+     * will be improved later.
+     */
+    @Column(length = 7)
+    private String gender = "unknown";
+
+    private LocalDate birthday;
+
+    @Column(length = 150)
+    private String description;
+
+    private Integer role = Const.Role.ROLE_NORMAL;
+
+    private LocalDateTime createTime = LocalDateTime.now();
+    private LocalDateTime updateTime;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+}
