99d5b86ada701698c72c7df8d6c548562620a0f1
happylrd <happylrd17@gmail.com>
2017-06-29 19:20:16 +0800

    First commit
    and add some config

--- Youbo/src/main/java/io/happylrd/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.happylrd.repository;
-
-import io.happylrd.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    User findByUsername(String username);
-}
--- Youbo/src/main/java/io/happylrd/service/impl/UserServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package io.happylrd.service.impl;
-
-import io.happylrd.service.UserService;
-
-public class UserServiceImpl implements UserService {
-}
