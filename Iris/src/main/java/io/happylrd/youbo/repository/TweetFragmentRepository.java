775a0a12f35ce9985d61c785202f8f290c5dcb9b
happylrd <happylrd17@gmail.com>
2017-07-07 14:26:22 +0800

    Remove unuseful repository

--- Iris/src/main/java/io/happylrd/youbo/repository/TweetFragmentRepository.java
+++
@@ -1,7 +0,0 @@
-package io.happylrd.youbo.repository;
-
-import io.happylrd.youbo.model.domain.TweetFragment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TweetFragmentRepository extends JpaRepository<TweetFragment, Long> {
-}
